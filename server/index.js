3c1df45f85e503a45e67122c6d2425fd2bdc0e4d
Marco Pineda <mapineda1222@gmail.com>
2021-03-13 16:32:00 -0600

    update server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
+// imports
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// init the app
+const app = express();
+// assign server port
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json());
+
+// idx route
+app.get('/', (req, res) => {
+    res.send('Salve Terra!')
+})
+
+// listen on port
+app.listen(PORT, ()=> {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
