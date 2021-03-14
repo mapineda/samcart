1be08f269d721e3dadca72fceefd85aa30580c69
Marco Pineda <mapineda1222@gmail.com>
2021-03-13 23:38:01 -0600

    updates index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 // imports
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = require('./db')
+const carRouter = require('./routes/car-router')
 
 // init the app
 const app = express();
@@ -12,10 +15,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json());
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 // idx route
 app.get('/', (req, res) => {
     res.send('Salve Terra!')
 })
+
+// api route
+app.use('/api', carRouter)
 
 // listen on port
 app.listen(PORT, ()=> {
