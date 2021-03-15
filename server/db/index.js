60ef687932a283fb0f9b5031270290b1ab0769ee
Marco Pineda <mapineda1222@gmail.com>
2021-03-14 20:36:00 -0500

    adds url for mongo atlas

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017/cars';
 
+const CONNECTION_STRING = 'mongodb+srv://samcart:<password>@cluster0.wfgek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
 mongoose
-.connect(connectionUrl, { useNewUrlParser: true })
+.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch((e) => {
     console.error(`Connection error: ${e.message}`);
 })
